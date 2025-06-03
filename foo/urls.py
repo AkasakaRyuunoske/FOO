0308e0a77b2f31a23181db73f0b79f47d87cd565
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2025-06-03 20:57:18 +0200

    aggiunto tailwind css; aggiunto base con un header/footer di esempio; aggiunta pagina discover vuota; aggiornamento degli url

--- foo/urls.py
+++ foo/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
     path('users/', include('UserManager.urls')),
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('discover/', views.discover, name='discover')
     # path('recipe/', include('RecipeManager.urls')),
     # path('tag/', include('TagManager.urls')),
     # path('ingredient/', include('IngredientManager.urls')),
