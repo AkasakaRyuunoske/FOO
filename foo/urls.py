1e423e3e5d3b1c6ccba544672a03722c62fc4f11
MaksymSavitskyi <maksym.savitskyi@idea75.it>
2025-07-02 17:03:19 +0200

    fix percorsi

--- foo/urls.py
+++ foo/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from common import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
