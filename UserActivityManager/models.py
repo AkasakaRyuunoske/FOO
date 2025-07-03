2dd80bf500d3a3e7fe9bba792e0a7565fb6f96c5
MaksymSavitskyi <maksym.savitskyi@idea75.it>
2025-07-03 12:10:48 +0200

    aggiunta entita clusterTag;

--- UserActivityManager/models.py
+++ UserActivityManager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from UserManager.models import CustomUser
+
+
+class ClusterTag(models.Model):
+    id = models.IntegerField(primary_key=True)
+    created_by_user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="clusters")
+    name = models.CharField(max_length=255, unique=True, null=False)
+    description = models.TextField(max_length=4000, null=False)
+
+    class Meta:
+        verbose_name = "Tag Cluster"
+        verbose_name_plural = "Tag Clusters"
+
+    def __str__(self): return self.name
