571d025d4176a3198ea85d818be80b7272985f56
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2025-07-09 21:37:48 +0200

    per ogni tag viene generata una card e a questa viene passato il tag dinamicamente

--- TagManager/views.py
+++ TagManager/views.py
@@ -17,9 +17,14 @@ def get_tags(request):
     return render(request, "components/tags_list.html", {"tags": result})
 
 def get_recipes_by_tags(request):
+    # prendo gli tag selezionati dal utente dalla query string
     tag_string = request.GET.get("tags", "")
 
-    return HttpResponse(tag_string, 200)
+    # pulizia degli tag
+    tags = [t.strip() for t in tag_string.split(",") if t.strip()]
+
+    # ritorno il componente che itera sugli tag e per ogni tag crea una card
+    return render(request, "components/recipe_cards.html", {"tags": tags})
 
 def get_ingredients(request):
     sleep(3)
