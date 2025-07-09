8671b8224b989acfa0b74aa99794bf55c028e1da
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2025-07-09 21:28:53 +0200

    aggiunta la funzionalita di passaggio degli tag al backend

--- TagManager/urls.py
+++ TagManager/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('tags/', views.get_tags, name='get_tags'),
+    path('get_recipes_by_tags/', views.get_recipes_by_tags, name='get_recipes_by_tags'),
     path('ingredients/', views.get_ingredients, name='get_ingredients'),
 ]
