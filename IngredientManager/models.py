ea49e76b08f368e2217cb7c38175b45e954fb879
cay3na <chloeeebrionesss@gmail.com>
2025-07-10 13:42:50 +0200

    changed models py of ingredient manager

--- IngredientManager/models.py
+++ IngredientManager/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+# ID - int, autoincrement, primary key
+# Name - varchar, not null, unique
+# Description - text, nullable
+
+class Ingredient(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255, null=False, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Ingredient"
+        verbose_name_plural = "Ingredients"
+
+    def __str__(self): return self.name
