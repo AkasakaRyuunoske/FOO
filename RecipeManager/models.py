9311338994eea7ce887a8dd20a3c0b3f29e48806
cay3na <chloeeebrionesss@gmail.com>
2025-07-11 20:16:00 +0200

    added rating class, did commands

--- RecipeManager/models.py
+++ RecipeManager/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -12,3 +13,19 @@ class Recipe(models.Model):
         verbose_name_plural = "Recipes"
 
     def __str__(self): return self.name
+
+
+class Rating(models.Model):
+    id = models.AutoField(primary_key=True)
+    stars = models.IntegerField(null=False, unique=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # points at CustomUser model
+        on_delete=models.CASCADE, db_column='user_id', related_name='ratings')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, db_column='recipe_id', related_name='ratings')
+
+    class Meta:
+        verbose_name = "Rating"
+        verbose_name_plural = "Ratings"
+
+    def __str__(self):
+        return f"{self.stars} stars by {self.user.username} on {self.recipe.name}"
