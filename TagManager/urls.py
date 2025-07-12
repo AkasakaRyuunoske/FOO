ab8ac5d67da50a87099ff697172cdbda1a768275
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2025-07-12 18:31:56 +0200

    aggiunta gestione dinamica degli tag nelle recipe card (da finire)

--- TagManager/urls.py
+++ TagManager/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('tags/', views.get_tags, name='get_tags'),
     path('get_recipes_by_tags/', views.get_recipes_by_tags, name='get_recipes_by_tags'),
+    path("search_tags/", views.search_tags, name="search_tags"),
     path('ingredients/', views.get_ingredients, name='get_ingredients'),
 ]
