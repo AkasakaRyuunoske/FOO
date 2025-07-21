ad6e714c0570c9e8bbcd4df99343aa86d4a0f304
alemassim0 <alessandromassimo04@gmail.com>
2025-07-21 18:10:10 +0200

    aggiunti campi description e icon nel model

--- TagManager/models.py
+++ TagManager/models.py
@@ -3,12 +3,16 @@ from RecipeManager.models import Recipe
 
 class TagType(models.Model):
     name = models.CharField(max_length=255, unique=True, null=False)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    icon = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=255, null=False)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    icon = models.CharField(max_length=255, null=True, blank=True)
     tag_type = models.ForeignKey(TagType, on_delete=models.CASCADE, related_name='tags', null=True)
 
     def __str__(self):
