38b07ae2a1aefca66ddaf73a6c38b69eca95a81a
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2025-07-21 18:44:51 +0200

    fix merge conflict

--- common/urls.py
+++ common/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-<<<<<<< HEAD
-=======
     path("recipes/", views.recipe_list, name="recipe_list"),
     path("recipe_details/", views.recipe_details, name='recipe_details'),
->>>>>>> 2ab48244d766e93b3110c4574d70ddd3bd1c973f
 ]
