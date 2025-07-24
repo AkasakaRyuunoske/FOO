df9c95d3b1f35007f1c4d6ecc20735c8d9cf7fb2
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2025-07-24 14:13:12 +0200

    aggiunto mock della pagina utente

--- UserManager/urls.py
+++ UserManager/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path("logout/", LogoutView.as_view(next_page="register"), name="logout"),
+    path("profile/", views.user_profile, name="user_profile"),
 ]
