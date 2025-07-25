9b2043fc85d65f93bf027901502625bd61fe2288
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2025-07-25 11:17:24 +0200

    relazione RecipeTag funziona

--- TagManager/models.py
+++ TagManager/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from RecipeManager.models import Recipe
+from django.db import models
+
 
 class TagType(models.Model):
     name = models.CharField(max_length=255, unique=True, null=False)
@@ -8,6 +9,7 @@ class TagType(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=255, null=False)
