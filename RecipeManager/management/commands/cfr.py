7cdab82b81fe486aed10a88c4f64d7ed75abdfc8
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2025-07-27 12:24:29 +0200

    fix dello script

--- RecipeManager/management/commands/cfr.py
+++ RecipeManager/management/commands/cfr.py
@@ -53,12 +53,12 @@ class Command(BaseCommand):
 
                     RecipeTag.objects.create(recipe=recipe, tag=tags["difficulty"][row["DIFFICULTY"].strip().lower()])
                     RecipeTag.objects.create(recipe=recipe, tag=tags["preparation time"][row["PREPARATION_TIME"].split('(')[0].strip().lower()])
-                    RecipeTag.objects.create(recipe=recipe, tag=tags["vegetarian"]["yes" if row["VEGETARIAN"] else "no"])
-                    RecipeTag.objects.create(recipe=recipe, tag=tags["vegan"]["yes" if row["VEGAN"] else "no"])
+                    RecipeTag.objects.create(recipe=recipe, tag=tags["vegetarian"]["yes" if row["VEGETARIAN"] == "true" else "no"])
+                    RecipeTag.objects.create(recipe=recipe, tag=tags["vegan"]["yes" if row["VEGAN"] == "true" else "no"])
                     RecipeTag.objects.create(recipe=recipe, tag=tags["cooking method"][row["COOKING_METHOD"].strip().lower()])
                     RecipeTag.objects.create(recipe=recipe, tag=tags["cost"][row["price_tag"].strip().lower()])
-                    RecipeTag.objects.create(recipe=recipe, tag=tags["lactose_free"][row["LACTOSE_FREE"].strip().lower()])
-                    RecipeTag.objects.create(recipe=recipe, tag=tags["gluten_free"][row["GLUTEN_FREE"].strip().lower()])
+                    RecipeTag.objects.create(recipe=recipe, tag=tags["lactose free"]["yes" if row["LACTOSE_FREE"] == "true" else "no"])
+                    RecipeTag.objects.create(recipe=recipe, tag=tags["gluten free"]["yes" if row["GLUTEN_FREE"] == "true" else "no"])
 
                 except Exception as exception:
                     self.stderr.write(f"Skipping row due to error: {exception}")
